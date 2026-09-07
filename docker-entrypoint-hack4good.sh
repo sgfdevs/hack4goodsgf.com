e67d7635f99b92342685769251af671fac18c018
Levi Zitting <me@levizitting.com>
2026-09-07 18:43:30 -0500

    fix: retain WP-CLI during container startup (#2)
    Remove the startup-time deletion of `/usr/local/bin/wp`, leaving WP-CLI
    available in the running container. All other initialization remains
    unchanged.

--- docker-entrypoint-hack4good.sh
+++ docker-entrypoint-hack4good.sh
@@ -3,6 +3,5 @@ set -eu
 
 mkdir -p /var/www/html/wp-content/mu-plugins
 cp /usr/src/wordpress/wp-content/mu-plugins/hack4good-smtp.php /var/www/html/wp-content/mu-plugins/hack4good-smtp.php
-rm -f /usr/local/bin/wp
 
 exec docker-entrypoint.sh "$@"
